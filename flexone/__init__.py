51c86d7e4ef86658292a46b786731a5c383da690
vijay_wm@yahoo.com <vijay_wm@yahoo.com>
2021-02-17 11:34:20 +0530

    added condition to update accounts_receivable_summary_cf if exists

--- flexone/__init__.py
+++ flexone/__init__.py
@@ -31,25 +31,26 @@ def after_migrate():
 
 
 def on_submit_sales_invoice(doc, method):
-    template = "flexone/flexone/print_format/accounts_receivable_summary.html"
-    from erpnext.accounts.report.accounts_receivable_summary.accounts_receivable_summary import (
-        execute,
-    )
+    if doc.meta.get_field("accounts_receivable_summary_cf"):
+        template = "flexone/flexone/print_format/accounts_receivable_summary.html"
+        from erpnext.accounts.report.accounts_receivable_summary.accounts_receivable_summary import (
+            execute,
+        )
 
-    filters = dict(
-        ageing_based_on="Posting Date",
-        company=doc.company,
-        customer=doc.customer,
-        report_date=doc.posting_date,
-        range1=30,
-        range2=60,
-        range3=90,
-        range4=120,
-    )
+        filters = dict(
+            ageing_based_on="Posting Date",
+            company=doc.company,
+            customer=doc.customer,
+            report_date=doc.posting_date,
+            range1=30,
+            range2=60,
+            range3=90,
+            range4=120,
+        )
 
-    _, summary = execute(filters)
-    summary = [summary[0]] if summary else []
-    accounts_receivable_summary_cf = frappe.render_template(
-        template, dict(summary=summary, doc=doc)
-    )
-    doc.accounts_receivable_summary_cf = accounts_receivable_summary_cf
+        _, summary = execute(filters)
+        summary = [summary[0]] if summary else []
+        accounts_receivable_summary_cf = frappe.render_template(
+            template, dict(summary=summary, doc=doc)
+        )
+        doc.accounts_receivable_summary_cf = accounts_receivable_summary_cf
